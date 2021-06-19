a847c02a4f6957fee68df1bb43b4dc1a34ec4787
rohilpinto <rohilcrispinto@gmail.com>
2021-06-19 23:12:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import logo from "./assets/enter-icon.svg";
 const SearchBar = () => {
   return (
     <React.Fragment>
-      <div className="title">
-        <h1>Card Notes</h1>
-      </div>
-      <div className="searchbar-container">
-        <form className="searchbar-form">
-          <input type="text" className="searchbar" />
-          <button type="submit" className="searchbar-submit">
-            <img src={logo} alt="button" />
-          </button>
-        </form>
+      <div className="searchbar-wrapper">
+        <div className="searchbar-title">
+          <h1>Card Notes</h1>
+        </div>
+        <div className="searchbar-container">
+          <form className="searchbar-form">
+            <input type="text" className="searchbar" />
+            <button type="submit" className="searchbar-submit">
+              <img src={logo} alt="button" />
+            </button>
+          </form>
+        </div>
       </div>
     </React.Fragment>
   );
