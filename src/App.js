145f9478810b06b83a095751697352777db4cc0d
rohilpinto <rohilcrispinto@gmail.com>
2021-06-20 21:20:28 +0530

    added UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./scss/App.scss";
 import logo from "./assets/enter-icon.svg";
+import leftarrow from "./assets/left-arrow.svg";
+import rightarrow from "./assets/right-arrow.svg";
 
 const SearchBar = () => {
   return (
@@ -26,15 +28,26 @@ const NotesCard = () => {
   return (
     <React.Fragment>
       <div className="card-container">
-        <div className="arrow arrow-left">Left</div>
-
-
-        <div className="card">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum nulla facilis natus mollitia veniam, libero itaque nam voluptates commodi fuga tempore sequi voluptate quo nemo atque possimus voluptatem! Pariatur, nobis.
+        <div className="arrow arrow-left">
+          <img src={leftarrow} alt="" />
         </div>
 
+        <div className="card"></div>
 
-        <div className="arrow arrow-right">Right</div>
+        <div className="arrow arrow-right">
+          <img src={rightarrow} alt="" />
+        </div>
+      </div>
+      <Random></Random>
+    </React.Fragment>
+  );
+};
+
+const Random = () => {
+  return (
+    <React.Fragment>
+      <div className="random-btn">
+        <button className="btn">Shuffle Notes</button>
       </div>
     </React.Fragment>
   );
